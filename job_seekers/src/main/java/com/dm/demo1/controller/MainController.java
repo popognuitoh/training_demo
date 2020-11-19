127ae0ba8ae75242c4fa4981bfedbea64a906660
jason <popognuitoh@ymail.com>
2020-11-19 11:11:05 +0800

    de

---
+++ job_seekers/src/main/java/com/dm/demo1/controller/MainController.java
@@ -0,0 +1,66 @@
+package com.dm.demo1.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
+
+/**
+ * ---------------------------
+ * (ReqDesignateWorkOrder) 这是我web层的第一个测试controller
+ * ---------------------------
+ *
+ * @Author: [hezhou]
+ * @Date: 2020/2/26
+ * @Version: [1.0.1]
+ * ---------------------------
+ */
+@Controller
+public class MainController {
+
+    /**
+     * 1.获取用户认证信息
+     * 哪里都可以获取 2.3只能在控制层
+     * @param map
+     * @return
+     */
+    @RequestMapping({"/index","/",""})
+    public String index(Map<String,Object> map){
+        // 会去找 resources/template/index.html
+        //1.拿到登陆后的用户的认证信息
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        //获取到的用户信息
+        Object result = authentication.getPrincipal();
+        if (result!=null && result instanceof UserDetails){
+           UserDetails userDetails =  (UserDetails)result;
+           String userName = userDetails.getUsername();
+           map.put("userName",userName);
+        }
+
+        return "index";
+    }
+
+    /**
+     *  2.拿到当前登陆的信息
+     */
+    @ResponseBody
+    @RequestMapping("/user/info")
+    public Object userInfo(Authentication authentication){
+        return authentication.getPrincipal();
+    }
+
+    /**
+     *  3.拿到当前登陆的信息
+     */
+    @ResponseBody
+    @RequestMapping("/user/info2")
+    public UserDetails userInfo2(@AuthenticationPrincipal UserDetails userDetails){
+        return userDetails;
+    }
+}
---
+++ job_seekers/src/main/java/com/dm/demo1/controller/SysPermissionController.java
@@ -0,0 +1,30 @@
+package com.dm.demo1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * ---------------------------
+ * (SysUserController) 系统权限控制器
+ * ---------------------------
+ *
+ * @Author: [hezhou]
+ * @Date: 2020/3/2
+ * @Version: [1.0.1]
+ * ---------------------------
+ */
+@Controller
+@RequestMapping("/permission")
+public class SysPermissionController {
+
+    private static final String HTML_PREFIX = "system/permission/";
+
+    @GetMapping(value = {"/","","/index"})
+    public String user()
+    {
+        System.out.println("je suis la haha");
+
+        return HTML_PREFIX + "permission-list 1";
+    }
+}
