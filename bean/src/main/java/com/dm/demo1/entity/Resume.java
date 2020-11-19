ec646206a43e97492c2debef0683f2d10d8a42e3
jason <popognuitoh@ymail.com>
2020-11-19 11:09:38 +0800

    de

---
+++ bean/src/main/java/com/dm/demo1/entity/Resume.java
@@ -0,0 +1,22 @@
+package com.dm.demo1.entity;
+
+public class Resume {
+    private AboutCity aboutCity;
+    private UserAccount userAccount;
+
+    public AboutCity getAboutCity() {
+        return aboutCity;
+    }
+
+    public void setAboutCity(AboutCity aboutCity) {
+        this.aboutCity = aboutCity;
+    }
+
+    public UserAccount getUserAccount() {
+        return userAccount;
+    }
+
+    public void setUserAccount(UserAccount userAccount) {
+        this.userAccount = userAccount;
+    }
+}
